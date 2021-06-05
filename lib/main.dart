1a73bc572659e6ca8aacdb280b48bcc0ef27aced
joshuaphilips <philipsjoshua96@gmail.com>
2021-06-05 15:35:30 +0000

    refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:crypto_cedi/home_page.dart';
+import 'package:crypto_cedi/pages/home_page.dart';
 import 'package:crypto_cedi/providers/theme_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
-import 'package:crypto_cedi/crypto_model.dart';
-import 'package:crypto_cedi/homepage_appbar.dart';
+import 'package:crypto_cedi/components/crypto_card.dart';
+import 'package:crypto_cedi/models/cryptocurrency.dart';
+import 'package:crypto_cedi/components/homepage_appbar.dart';
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 
@@ -128,53 +129,5 @@ class _HomePageState extends State<HomePage> {
         child: CircularProgressIndicator(),
       );
     }
-  }
-}
-
-class CryptoCard extends StatelessWidget {
-  final Cryptocurrency cryptocurrency;
-  final double cediExchangeRate;
-
-  const CryptoCard({Key key, this.cryptocurrency, this.cediExchangeRate})
-      : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.only(top: 8.0, bottom: 8),
-        child: ListTile(
-          contentPadding: EdgeInsets.only(left: 8, right: 16),
-          leading: CircleAvatar(
-            foregroundImage: NetworkImage(cryptocurrency.image),
-            radius: 35,
-          ),
-          title: Text(
-            cryptocurrency.name,
-          ),
-          subtitle: Text(
-            cryptocurrency.symbol.toUpperCase(),
-          ),
-          trailing: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                '¢${(cryptocurrency.currentPrice * cediExchangeRate).toStringAsFixed(2)}',
-              ),
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text(
-                    '${(cryptocurrency.priceChangePercentage24h?.toStringAsFixed(2))} %',
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
   }
 }
